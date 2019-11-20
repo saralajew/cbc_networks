616b7f0a9d8085f6fd71ddd9f72022ca9f292f5c
EmilioOldenziel <emilio.oldenziel@gmail.com>
2019-11-20 17:27:59 +0100

    test margin loss

--- keras_cbc/tests.py
+++ keras_cbc/tests.py
@@ -3,10 +3,13 @@ import pytest
 import numpy as np
 import tensorflow as tf
 
+import keras.backend as K
+
 from keras.layers import Input
 from keras.models import Model
 
 from keras_cbc.layers.detection_probability_functions import CosineSimilarity2D
+from keras_cbc.utils.losses import MarginLoss
 
 
 def test_cosine_similarity_2d():
@@ -28,7 +31,13 @@ def test_cosine_similarity_2d():
 
         assert out.shape == (1, 1, 1, 1)
 
-        # batch/components, W, H, channels
+    # batch/components, W, H, channels
     cos(np.ones((1, 24, 24, 3)),            np.ones((1, 24, 24, 3)))
     cos(np.zeros((1, 24, 24, 3)),           np.zeros((1, 24, 24, 3)))
     cos(np.random.random((1, 24, 24, 1)),   np.random.random((1, 24, 24, 1)))
+
+def test_margin_loss():
+    loss_fn = MarginLoss()
+    y_true = K.constant([4, 8, 12, 8, 1, 3], shape=(1,6))
+    loss = loss_fn(y_true, y_true)
+    assert np.isclose(K.eval(loss), 0.0)
